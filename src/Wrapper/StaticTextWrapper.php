22175a8f7b4488519b7c2a1ed36a3190faba28d2
plutuss <dima92konovalenko@gmail.com>
2024-05-02 09:36:44 +0200

    add: Wrapper

---
+++ src/Wrapper/StaticTextWrapper.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Plutuss\Wrapper;
+
+class StaticTextWrapper
+{
+    private ?string $name;
+    private ?int $page_id;
+    private array $data;
+
+    /**
+     * @param string $name
+     * @return $this
+     */
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @param int $page_id
+     * @return $this
+     */
+    public function setPageId(int $page_id): static
+    {
+        $this->page_id = $page_id;
+        return $this;
+    }
+
+    /**
+     * @param string $key
+     * @param string $value
+     * @param string $type
+     * @return $this
+     */
+    public function setData(string $key, string $value, string $type): static
+    {
+        $data = [
+            [
+                "id" => 1,
+                "key" => $key,
+                "value" => $value,
+                "type" => $type,
+            ],
+
+        ];
+        if (!empty($this->data)) {
+            $last = end($this->data);
+            $id = $last['id'];
+            $data[0]['id'] = ++$id;
+            $this->data = array_merge($this->data, $data);
+            return $this;
+        }
+
+        $this->data = $data;
+        return $this;
+    }
+
+    /**
+     * @return array{name: string, page_id: int, data: array}
+     */
+    public function get(): array
+    {
+        $data = $this->data;
+        $name = $this->name;
+        $page_id = $this->page_id;
+
+        unset($this->data);
+        unset($this->name);
+        unset($this->page_id);
+
+        return [
+            'name' => $name,
+            'page_id' => $page_id,
+            'data' => $data
+        ];
+
+    }
+
+}
